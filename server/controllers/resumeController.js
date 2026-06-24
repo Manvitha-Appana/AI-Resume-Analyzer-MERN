e9b671c1c5bd9a3c26853a1f8d03af508664e7d1
Manvitha-Appana <appanamanvitha14@gmail.com>
2026-06-24 10:52:49 +0530

    Added PDF text extraction and resume parsing

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -1,17 +1,33 @@
 const Resume = require("../models/Resume");
+const fs = require("fs");
+const pdfParse = require("pdf-parse");
+//console.log("PDF PACKAGE:", pdf);
+
 
 const uploadResume = async (req, res) => {
   try {
+    const pdfBuffer = fs.readFileSync(
+      req.file.path
+    );
+
+    const pdfData = await pdfParse(
+      pdfBuffer
+    );
+
     const resume = await Resume.create({
       fileName: req.file.filename,
       filePath: req.file.path,
+      extractedText: pdfData.text,
     });
 
     res.status(201).json({
-      message: "Resume uploaded successfully",
+      message:
+        "Resume uploaded and parsed successfully",
       resume,
     });
   } catch (error) {
+    console.log(error);
+
     res.status(500).json({
       message: error.message,
     });
