6afa2888d2b3af7506d492edeef8710d10b72099
Manvitha-Appana <appanamanvitha14@gmail.com>
2026-06-24 09:44:15 +0530

    Added login API and JWT authentication

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
@@ -12,10 +13,15 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(
+  "/api/auth",
+  authRoutes
+);
 
 app.get("/", (req, res) => {
   res.send("AI Resume Analyzer API Running");
 });
+
 
 const PORT = process.env.PORT || 5000;
 
